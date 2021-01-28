721edd282c5eed4c5dd96cf13e8e1cded5b2b7fc
Vishnu Ravi <vishnu.ravi@gmail.com>
2021-01-27 19:50:27 -0500

    Update to CardinalKit release 1.2.0 (#5)
    Co-authored-by: Varun Shenoy <vnshenoy@stanford.edu>
    Co-authored-by: ApolloZhu <public-apollonian@outlook.com>
    Co-authored-by: Lucas W <50314114+Lucas-ZX-W@users.noreply.github.com>
    Co-authored-by: Santiago Gutierrez <ssgutierrez42@gmail.com>

---
+++ CardinalKit-Example/CardinalKit/Components/Tasks/Model/TaskItem.swift
@@ -0,0 +1,143 @@
+//
+//  StudyTableItem.swift
+//
+//  Created for the CardinalKit Framework.
+//  Copyright © 2019 Stanford University. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import ResearchKit
+import SwiftUI
+
+enum TaskItem: Int {
+
+    /*
+     * STEP (1) APPEND TABLE ITEMS HERE,
+     * Give each item a recognizable name!
+     */
+    case //sampleResearchKitSurvey,
+         //sampleResearchKitActiveTask,
+         //sampleCoreMotionAppleWatch,
+         // sampleFunCoffeeSurvey,
+         resources,
+         dashboard,
+         backgroundSurvey,
+         survey
+    
+    /*
+     * STEP (2) for each item, what should its
+     * title on the list be?
+     */
+    var title: String {
+        switch self {
+        case .backgroundSurvey:
+            return "Background Questionnaire"
+        case .survey:
+            return "Symptom Questionnaire"
+        //case .sampleCoreMotionAppleWatch:
+           // return "Sensors Demo"
+//        case .sampleFunCoffeeSurvey:
+//            return "Coffee Survey"
+        case .resources:
+            return "Patient Resources"
+        case .dashboard:
+            return "Data Dashboard"
+        }
+    }
+    
+    /*
+     * STEP (3) do you need a subtitle?
+     */
+    var subtitle: String {
+        switch self {
+        case .backgroundSurvey:
+            return "One-time demographic questionnaire"
+        case .survey:
+            return "Weekly symptom reporting"
+       // case .sampleCoreMotionAppleWatch:
+         //   return "CoreMotion & Cloud Storage"
+//        case .sampleFunCoffeeSurvey:
+//            return "How do you like your coffee?"
+        case .resources:
+            return "Support for patients"
+        case .dashboard:
+            return "Summary of your data"
+        }
+    }
+    
+    /*
+     * STEP (4) what image would you like to associate
+     * with this item under the list view?
+     * Check the Assets directory.
+     */
+    var image: UIImage? {
+        switch self {
+        case .backgroundSurvey:
+            return getImage(named: "ActivityIcon")
+//        case .sampleFunCoffeeSurvey:
+//            return getImage(named: "CoffeeIcon")
+       // case .sampleCoreMotionAppleWatch:
+         //   return getImage(named: "WatchIcon")
+        case .resources:
+            return getImage(named: "CKLogoIcon")
+        default:
+            return getImage(named: "SurveyIcon")
+        }
+    }
+    
+    /*
+     * STEP (5) what section should each item be under?
+     */
+    var section: String {
+        switch self {
+        case .backgroundSurvey, .survey:
+            return "Current Tasks"
+//        case .sampleFunCoffeeSurvey:
+//            return "Your Interests"
+        case .resources, .dashboard:
+            return "Learn"
+        }
+    }
+
+    /*
+     * STEP (6) when each element is tapped, what should happen?
+     * define a SwiftUI View & return as AnyView.
+     */
+    var action: some View {
+        switch self {
+        case .backgroundSurvey:
+            return AnyView(CKTaskViewController(tasks: StudyTasks.backgroundTask))
+        case .survey:
+            return AnyView(CKTaskViewController(tasks: StudyTasks.sf12Task))
+        //case .sampleCoreMotionAppleWatch:
+          //  return AnyView(SensorsDemoUIView())
+//        case .sampleFunCoffeeSurvey:
+//            return AnyView(CKTaskViewController(tasks: TaskSamples.sampleCoffeeTask))
+        case .resources:
+            return AnyView(ResourcesView())
+        case .dashboard:
+            return AnyView(DashboardView())
+        }
+    }
+    
+    /*
+     * HELPERS
+     */
+    
+    fileprivate func getImage(named: String) -> UIImage? {
+        UIImage(named: named) ?? UIImage(systemName: "questionmark.square")
+    }
+    
+    static var allValues: [TaskItem] {
+        var index = 0
+        return Array (
+            AnyIterator {
+                let returnedElement = self.init(rawValue: index)
+                index = index + 1
+                return returnedElement
+            }
+        )
+    }
+    
+}
