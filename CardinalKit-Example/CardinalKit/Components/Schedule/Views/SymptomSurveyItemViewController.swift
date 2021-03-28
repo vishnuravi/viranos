85e9035a3389564049a02cdaaa3d1d02b668955d
Ashley Griffin <griffinac1@gmail.com>
2021-03-28 13:43:18 -0700

    UI updates

---
+++ CardinalKit-Example/CardinalKit/Components/Schedule/Views/SymptomSurveyItemViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SymptomSurveyItemViewController.swift
+//  CardinalKit_Example
+//
+//  Created by Ashley on 3/27/21.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import Foundation
